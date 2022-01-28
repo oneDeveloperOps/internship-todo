dc803b680d25cc91d8f92849bc801a272658cc3d
DeveloperOps <manidecp@gmail.com>
2022-01-28 16:30:34 +0530

    minor update

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express();
+
+app.use("/", (req, res) => {
+    res.send("<h1>Hello</h1>");
+})
+
+app.listen(3000, () => {
+    console.log("Application running on port 3000")
+})
